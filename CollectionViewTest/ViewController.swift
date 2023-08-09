beaf64c08f60bc57e792449f0a9b64449bd0b22c
hongssup <hongssup0202@gmail.com>
2023-08-09 20:27:21 +0900

    Initial Commit

---
+++ CollectionViewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewTest
+//
+//  Created by SeoYeon Hong on 2023/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
